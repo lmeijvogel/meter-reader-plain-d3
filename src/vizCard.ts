250defda6e46d625786bb4db225dcb8937da53aa
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-11-02 21:09:23 +0100

    Boyscout: Append created row to document in 'createRow()'.

--- src/vizCard.ts
+++ src/vizCard.ts
@@ -1,20 +1,17 @@
-export function addCards(rowsAndIds: string[][], container: HTMLElement) {
-    for (let ids of rowsAndIds) {
-        const row = createRow();
-        row.className = "row";
-
-        for (let id of ids) {
-            createSvgCard(id, row);
-        }
-
-        container.append(row);
-    }
-}
-
 export function setCardTitle(selection: d3.Selection<d3.BaseType, unknown, HTMLElement, any>, title: string) {
     const titleElement = selection.select(".title");
 
     titleElement.text(title);
+}
+
+export function addCards(rowsAndIds: string[][], container: HTMLElement) {
+    for (let ids of rowsAndIds) {
+        const row = createRow(container);
+
+        for (let id of ids) {
+            createSvgCard(id, row);
+        }
+    }
 }
 
 function createSvgCard(id: string, row: HTMLElement) {
@@ -33,18 +30,20 @@ function createSvgCard(id: string, row: HTMLElement) {
 
     row.append(div);
 
-    div.addEventListener("click", (e: any) => {
-        console.log("card clicked", e);
-    });
+    // div.addEventListener("click", (e: any) => {
+    // console.log("card clicked", e);
+    // });
 
-    div.getElementsByClassName("overlay")[0].addEventListener("click", (e) => {
-        console.log("overlay clicked", e);
-    });
+    // div.getElementsByClassName("overlay")[0].addEventListener("click", (e) => {
+    // console.log("overlay clicked", e);
+    // });
 }
 
-function createRow() {
+function createRow(container: HTMLElement) {
     const div = document.createElement("div");
     div.className = "row";
+
+    container.append(div);
 
     return div;
 }
