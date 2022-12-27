bd003639265a12d7e46e3f7b7b77b59d57487270
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-12-27 22:43:37 +0100

    Split water prices into separate list...
    Water rates are determined by a different company, so
    it doesn't make sense to link them to the energy rate
    validity periods.

--- src/helpers/PriceCalculator.ts
+++ src/helpers/PriceCalculator.ts
@@ -18,9 +18,14 @@ export class Money {
     }
 }
 
-type RateForDateRange = {
+type EnergyRateForDateRange = {
     gasPrice: Money;
     stroomPrice: Money;
+    validFrom: Date;
+    validUntil: Date;
+};
+
+type WaterRateForDateRange = {
     waterPrice: Money;
     validFrom: Date;
     validUntil: Date;
@@ -31,27 +36,22 @@ type RateForDateRange = {
 // rates won't be worth the effort :D
 //
 // All rates are in given in euros, but our Money class wants cents.
-const prices: RateForDateRange[] = [
+const energyPrices: EnergyRateForDateRange[] = [
     {
         gasPrice: new Money(0.267),
         stroomPrice: new Money(0.047),
-        waterPrice: new Money(0),
         validFrom: new Date(2014, 0, 1),
         validUntil: new Date(2018, 10, 11)
     },
     {
         gasPrice: new Money(0.75336),
         stroomPrice: new Money(0.22652),
-        // Incl. BTW. Rate is given on the Evides website in per m3, but I'd like to show in liters, so divide by 1000.
-        waterPrice: new Money(1.30037 / 1000),
         validFrom: new Date(2018, 10, 12),
         validUntil: new Date(2019, 11, 25) // 25 december: End of current contract
     },
     {
         gasPrice: new Money(0.68358),
         stroomPrice: new Money(0.22035),
-        // Incl. BTW. Rate is given on the Vandebron website in per m3, but I'd like to show in liters, so divide by 1000.
-        waterPrice: new Money(1.30037 / 1000),
         validFrom: new Date(2019, 11, 26),
         validUntil: new Date(2022, 11, 26) // 25 december: End of current contract
     },
@@ -61,31 +61,55 @@ const prices: RateForDateRange[] = [
     {
         gasPrice: new Money(1.93352 + 0.02988 + 0.00515 + 0.39591 + 0.09428),
         stroomPrice: new Money(0.61864), // This is the "enkeltarief" price.
-        // Incl. BTW. Rate is given on the Vandebron website in per m3, but I'd like to show in liters, so divide by 1000.
-        waterPrice: new Money(1.30037 / 1000),
         validFrom: new Date(2022, 11, 26),
         validUntil: new Date(2023, 2, 31) // 1 april: Variable prices
     }
 ];
+// Incl. BTW. Rate is given on the Vandebron website in per m3, but I'd like to show in liters, so divide by 1000.
+const waterPrices: WaterRateForDateRange[] = [
+    {
+        waterPrice: new Money((0.85 + 0.348) / 1000), // Not actually known
+        validFrom: new Date(2014, 0, 1),
+        validUntil: new Date(2018, 10, 11)
+    },
+    {
+        waterPrice: new Money((0.85 + 0.348) / 1000),
+        validFrom: new Date(2020, 10, 16),
+        validUntil: new Date(2020, 11, 31)
+    },
+    {
+        waterPrice: new Money((0.859 + 0.354) / 1000),
+        validFrom: new Date(2020, 0, 1),
+        validUntil: new Date(2021, 11, 31)
+    },
+    {
+        waterPrice: new Money((0.863 + 0.35) / 1000),
+        validFrom: new Date(2022, 0, 1),
+        validUntil: new Date(2024, 11, 31) // Not known yet
+    }
+];
 
 export function costsFor(units: number, priceCategory: PriceCategory, date: Date): Money {
-    const currentRate = rateForDate(date);
+    if (priceCategory === "water") {
+        return rateForDate(date, waterPrices).waterPrice.multiply(units);
+    }
+
+    const currentRate = rateForDate(date, energyPrices);
 
     switch (priceCategory) {
         case "gas":
             return currentRate.gasPrice.multiply(units);
         case "stroom":
             return currentRate.stroomPrice.multiply(units);
-        case "water":
-            return currentRate.waterPrice.multiply(units);
         default:
             return assertNever(priceCategory);
     }
 }
 
-function rateForDate(date: Date): RateForDateRange {
-    const result = prices.filter((price) => price.validFrom <= date && date < price.validUntil);
+function rateForDate<T extends { validFrom: Date; validUntil: Date }>(date: Date, input: T[]): T {
+    const result = input.filter((price) => price.validFrom <= date && date < price.validUntil);
 
+    /* TODO: This type is awful! */
     if (result.length === 0) {
         console.error("No prices specified for the selected date");
         return {
@@ -94,7 +118,7 @@ function rateForDate(date: Date): RateForDateRange {
             waterPrice: new Money(0),
             validFrom: new Date(2014, 0, 1),
             validUntil: new Date(2038, 0, 1)
-        };
+        } as unknown as T;
     }
 
     return result[0];
