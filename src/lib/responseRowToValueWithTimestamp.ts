753e955a14195baaa3cb7ce89c0d12050bcb5894
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2022-12-30 21:53:04 +0100

    Filter out negative measurement values...
    There is one when we moved house:
    The new meter started at 0, causing a
    huge negative value, since I only log
    the absolute readings and make them
    negative later on.

--- src/lib/responseRowToValueWithTimestamp.ts
+++ src/lib/responseRowToValueWithTimestamp.ts
@@ -1,9 +1,10 @@
 import { ValueWithTimestamp } from "../models/ValueWithTimestamp";
 
-// TODO: Any
-export function responseRowToValueWithTimestamp(row: any): ValueWithTimestamp {
+type Row = [string, number];
+
+export function responseRowToValueWithTimestamp(row: Row): ValueWithTimestamp {
     return {
         timestamp: new Date(Date.parse(row[0])),
-        value: row[1]
+        value: Math.max(0, row[1])
     };
 }
