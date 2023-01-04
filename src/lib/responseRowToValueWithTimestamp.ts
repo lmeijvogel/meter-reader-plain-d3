f7c3cbf8aa29273565b874c303f0848969f7ac4d
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-01-04 15:39:55 +0100

    Reverse clamping readings to 0...
    ... Because current usage can be larger than 0.

--- src/lib/responseRowToValueWithTimestamp.ts
+++ src/lib/responseRowToValueWithTimestamp.ts
@@ -5,6 +5,6 @@ type Row = [string, number];
 export function responseRowToValueWithTimestamp(row: Row): ValueWithTimestamp {
     return {
         timestamp: new Date(Date.parse(row[0])),
-        value: Math.max(0, row[1])
+        value: Number(row[1])
     };
 }
