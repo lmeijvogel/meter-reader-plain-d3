cb1b1f6ab5f46ffbe83d054db78d7137a661c5cd
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-01-28 10:06:48 +0100

    Convert app from Parcel to Vite...
    Parcel had problems regenerating pages when
    changes were made.

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:4567",
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
+});
