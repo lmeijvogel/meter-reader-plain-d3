b48caa2a22d96215f56d929e678125621e2fc34d
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-02-23 21:58:37 +0100

    Boyscout: Extract functions from padData.ts.

--- src/lib/padData.ts
+++ src/lib/padData.ts
@@ -9,56 +9,65 @@ export function padData(
     startDate: Date,
     unitSize: "hour" | "day" | "month"
 ): ValueWithTimestamp[] {
-    const result: ValueWithTimestamp[] = [];
-
     if (unitSize === "hour") {
-        let currentDate: Date = startDate;
-
-        /* This seems like a roundabout way of iterating over the hours in the day,
-         * but it's better to do it like this (keep adding hours) instead of just going
-         * over hour 0 to 23 because of Daylight Savings Time: The boundary dates have
-         * 1 fewer or 1 more hour.
-         */
-        const dateAfter = addDays(startDate, 1);
-        do {
-            const existingElement = data.find(
-                (element) =>
-                    element.timestamp.getHours() === currentDate.getHours() &&
-                    element.timestamp.getTimezoneOffset() === currentDate.getTimezoneOffset() &&
-                    isSameDay(element.timestamp, startDate)
-            );
-
-            if (existingElement) {
-                result.push(existingElement);
-            } else {
-                result.push({ timestamp: currentDate, value: 0 });
-            }
-
-            currentDate = addHours(currentDate, 1);
-        } while (isBefore(currentDate, dateAfter));
-
-        return result;
+        return padHourlyData(startDate, data);
     }
 
     if (unitSize === "day") {
-        for (let day = 0; day < getDaysInMonth(startDate); day++) {
-            const currentDate = addDays(startDate, day);
-
-            const existingElement = data.find(
-                (element) =>
-                    element.timestamp.getDate() === day + 1 && element.timestamp.getMonth() === startDate.getMonth()
-            );
-
-            if (existingElement) {
-                result.push(existingElement);
-            } else {
-                result.push({ timestamp: currentDate, value: 0 });
-            }
-        }
-
-        return result;
+        return padDailyData(startDate, data);
     }
 
     // For year data, don't pad anything.
     return data;
+}
+
+function padHourlyData(startDate: Date, data: ValueWithTimestamp[]) {
+    const result: ValueWithTimestamp[] = [];
+    let currentDate: Date = startDate;
+
+    /* This seems like a roundabout way of iterating over the hours in the day,
+     * but it's better to do it like this (keep adding hours) instead of just going
+     * over hour 0 to 23 because of Daylight Savings Time: The boundary dates have
+     * 1 fewer or 1 more hour.
+     */
+    const dateAfter = addDays(startDate, 1);
+    do {
+        const existingElement = data.find(
+            (element) =>
+                element.timestamp.getHours() === currentDate.getHours() &&
+                element.timestamp.getTimezoneOffset() === currentDate.getTimezoneOffset() &&
+                isSameDay(element.timestamp, startDate)
+        );
+
+        if (existingElement) {
+            result.push(existingElement);
+        } else {
+            result.push({ timestamp: currentDate, value: 0 });
+        }
+
+        currentDate = addHours(currentDate, 1);
+    } while (isBefore(currentDate, dateAfter));
+
+    return result;
+}
+
+function padDailyData(startDate: Date, data: ValueWithTimestamp[]) {
+    const result: ValueWithTimestamp[] = [];
+
+    for (let day = 0; day < getDaysInMonth(startDate); day++) {
+        const currentDate = addDays(startDate, day);
+
+        const existingElement = data.find(
+            (element) =>
+                element.timestamp.getDate() === day + 1 && element.timestamp.getMonth() === startDate.getMonth()
+        );
+
+        if (existingElement) {
+            result.push(existingElement);
+        } else {
+            result.push({ timestamp: currentDate, value: 0 });
+        }
+    }
+
+    return result;
 }
